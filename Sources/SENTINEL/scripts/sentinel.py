9f08ee9b1525706a9165c093deface8b7c59851e
Sai Krishna <saikrishnadammalapati@gmail.com>
2023-07-31 14:42:02 +0530

    Update SENTINEL - District Transformer - Choropleth

--- Sources/SENTINEL/scripts/sentinel.py
+++ Sources/SENTINEL/scripts/sentinel.py
@@ -2,16 +2,48 @@ import ee
 import geemap
 import os 
 import time
-
-date_start = '2022-01-01'
-date_end ='2022-02-01'
-
-tic = time.perf_counter()
-cwd = os.getcwd()
+import rasterio
+import geopandas as gpd
+import rasterstats
+import pandas as pd
 
 service_account = ' idsdrr@ee-idsdrr.iam.gserviceaccount.com'
 credentials = ee.ServiceAccountCredentials(service_account, 'Sources/SENTINEL/ee-idsdrr-d856f70748a7.json')
 ee.Initialize(credentials)
+
+date_start = '2022-02-01'
+date_end ='2022-03-01'
+
+tic = time.perf_counter()
+cwd = os.getcwd()
+assam_rc_gdf = gpd.read_file(cwd+'/Maps/Assam_Revenue_Circles/assam_revenue_circle_nov2022.shp')
+assam_dist_gdf = gpd.read_file(cwd+'/Maps/assam_district_35.geojson')
+
+def zonal_stats_choropleths(gdf,
+                            gdf_unique_id,
+                            raster,
+                            stats_list=['mean', 'count'],
+                            ):
+    mean_dicts = rasterstats.zonal_stats(gdf.to_crs(raster.crs),
+                                              raster.read(1),
+                                              affine= raster.transform,
+                                              stats= stats_list,
+                                              nodata=raster.nodata,
+                                              geojson_out = True)
+    dfs = []
+    for rc in mean_dicts:
+        dfs.append(pd.DataFrame([rc['properties']]))
+        
+    zonal_stats_df = pd.concat(dfs).reset_index(drop=True)
+    
+    zonal_stats_gdf = pd.merge(gdf,
+                               zonal_stats_df[stats_list+[gdf_unique_id]],
+                               on=gdf_unique_id)
+    
+    zonal_stats_gdf = gpd.GeoDataFrame(zonal_stats_gdf)
+
+    return zonal_stats_df, zonal_stats_gdf
+
 
 # clip_image function clips the satellite image to our given area of interest
 def clip_image(aoi):
@@ -60,25 +92,60 @@ geemap.ee_export_image(ndvi,
                        scale=1000,
                        region=geometry,
                        file_per_band=True)
-print("-------NDBI Image-------------")
 
+print("-------NDBI Image-------------")
 geemap.ee_export_image(ndbi,
                        filename=cwd+'/Sources/SENTINEL/data/ndbi_{}.tif'.format(date_end),
                        scale=1000,
                        region=geometry,
                        file_per_band=True)
-print("-------NDVI Stats-------------")
 
-geemap.zonal_statistics(ndvi,
-                        assam_rcs,
-                        cwd+'/Sources/SENTINEL/data/ndvi_{}.csv'.format(date_end),
-                        statistics_type='MEAN',
-                        scale=1000)
+print("-------NDVI Stats-------------")
+# # geemap.zonal_statistics(ndvi,
+# #                         assam_rcs,
+# #                         cwd+'/Sources/SENTINEL/data/ndvi_{}.csv'.format(date_end),
+# #                         statistics_type='MEAN',
+# #                         scale=1000)
+ndvi_raster = rasterio.open(cwd+'/Sources/SENTINEL/data/ndvi_{}.ndvi.tif'.format(date_end))
+
+ndvi_rc_df, ndvi_rc_gdf = zonal_stats_choropleths(assam_rc_gdf,'object_id',
+                                                  ndvi_raster)
+
+ndvi_dist_df, ndvi_dist_gdf = zonal_stats_choropleths(assam_dist_gdf, 'assam_dist',
+                                                  ndvi_raster)
+
+ndvi_rc_df.to_csv(cwd+'/Sources/SENTINEL/data/ndvi_rc_{}.csv'.format(date_end),
+                index=False)
+ndvi_rc_gdf.to_file(cwd+'/Sources/SENTINEL/data/ndvi_rc_{}.geojson'.format(date_end))
+
+
+ndvi_dist_df.to_csv(cwd+'/Sources/SENTINEL/data/ndvi_dist_{}.csv'.format(date_end),
+                index=False)
+ndvi_dist_gdf.to_file(cwd+'/Sources/SENTINEL/data/ndvi_dist_{}.geojson'.format(date_end))
+
+
 print("-------NDBI Stats-------------")
-geemap.zonal_statistics(ndbi,
-                        assam_rcs,
-                        cwd+'/Sources/SENTINEL/data/ndbi_{}.csv'.format(date_end),
-                        statistics_type='MEAN',
-                        scale=1000)
+# geemap.zonal_statistics(ndbi,
+#                         assam_rcs,
+#                         cwd+'/Sources/SENTINEL/data/ndbi_{}.csv'.format(date_end),
+#                         statistics_type='MEAN',
+#                         scale=1000)
+
+ndbi_raster = rasterio.open(cwd+'/Sources/SENTINEL/data/ndbi_{}.ndbi.tif'.format(date_end))
+ndbi_rc_df, ndbi_rc_gdf = zonal_stats_choropleths(assam_rc_gdf,'object_id',
+                                                  ndbi_raster)
+
+ndbi_dist_df, ndbi_dist_gdf = zonal_stats_choropleths(assam_dist_gdf, 'assam_dist',
+                                                  ndbi_raster)
+
+ndbi_rc_df.to_csv(cwd+'/Sources/SENTINEL/data/ndbi_rc_{}.csv'.format(date_end),
+                index=False)
+ndbi_rc_gdf.to_file(cwd+'/Sources/SENTINEL/data/ndbi_rc_{}.geojson'.format(date_end))
+
+
+ndbi_dist_df.to_csv(cwd+'/Sources/SENTINEL/data/ndbi_dist_{}.csv'.format(date_end),
+                index=False)
+ndbi_dist_gdf.to_file(cwd+'/Sources/SENTINEL/data/ndbi_dist_{}.geojson'.format(date_end))
+
 toc = time.perf_counter()
 print("Time Taken: {} seconds".format(toc-tic))

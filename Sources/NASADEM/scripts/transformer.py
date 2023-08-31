eedde1c5f3e4ead72fccc1cb37d17fe66bd3b41c
Sai Krishna <saikrishnadammalapati@gmail.com>
2023-08-31 16:20:25 +0530

    Update NASADEM -Slope -Laregfile ignored

--- Sources/NASADEM/scripts/transformer.py
+++ Sources/NASADEM/scripts/transformer.py
@@ -25,20 +25,24 @@ for rc in mean_dicts:
 dem_zonal_stats_df = pd.concat(dfs).reset_index(drop=True)
 dem_zonal_stats_df = dem_zonal_stats_df.rename(columns={'mean':'elevation_mean'})
 
-dem_zonal_stats_df.to_csv(path+"data/NASADEM.csv")
-# slope_raster = rasterio.open(path+'/data/NASADEM_SLOPE_30.tif')
-# slope_raster_array = slope_raster.read(1)
+slope_raster = rasterio.open(path+'/data/NASADEM_SLOPE_30.tif')
+slope_raster_array = slope_raster.read(1)
 
-# mean_dicts = rasterstats.zonal_stats(assam_rc_gdf.to_crs(slope_raster.crs),
-#                                      slope_raster_array,
-#                                      affine= slope_raster.transform,
-#                                      stats= ['mean'],
-#                                      nodata=slope_raster.nodata,
-#                                      geojson_out = True)
-# dfs = []
-# for rc in mean_dicts:
-#     dfs.append(pd.DataFrame([rc['properties']]))
+mean_dicts = rasterstats.zonal_stats(assam_rc_gdf.to_crs(slope_raster.crs),
+                                     slope_raster_array,
+                                     affine= slope_raster.transform,
+                                     stats= ['mean'],
+                                     nodata=slope_raster.nodata,
+                                     geojson_out = True)
+dfs = []
+for rc in mean_dicts:
+    dfs.append(pd.DataFrame([rc['properties']]))
 
-# slope_zonal_stats_df = pd.concat(dfs).reset_index(drop=True)
-# slope_zonal_stats_df = slope_zonal_stats_df.rename(columns={'mean':'slope_mean'})
+slope_zonal_stats_df = pd.concat(dfs).reset_index(drop=True)
+slope_zonal_stats_df = slope_zonal_stats_df.rename(columns={'mean':'slope_mean'})
 
+zonal_stats_df = pd.merge(dem_zonal_stats_df,
+                          slope_zonal_stats_df[['object_id','slope_mean']],
+                          on='object_id')
+
+zonal_stats_df.to_csv(path+"data/NASADEM.csv", index=False)

ca033939cd301e391e2840973e1c200be0718158
Sai Krishna <saikrishnadammalapati@gmail.com>
2023-09-18 12:27:10 +0530

    UPDATE BHARATMAPS road rail lenghts

--- Sources/BHARATMAPS/scripts/healthcentres_per_rc.py
+++ Sources/BHARATMAPS/scripts/healthcentres_per_rc.py
@@ -3,10 +3,10 @@ import geopandas as gpd
 import os
 
 cwd = os.getcwd()
-health_centres_gdf = gpd.read_file(cwd+'/Sources/BHARATMAPS/data/BharatMaps_HealthCenters.geojson')
+health_centres_gdf = gpd.read_file(cwd+'/Sources/BHARATMAPS/data/RawData/BharatMaps_HealthCenters.geojson')
 assam_rc_gdf = gpd.read_file(cwd+'/Maps/Assam_Revenue_Circles/assam_revenue_circle_nov2022.shp')
 
 health_centres_in_rcs = gpd.sjoin(assam_rc_gdf, health_centres_gdf.to_crs(assam_rc_gdf.crs), how="left", predicate="contains")
 health_centres_count = health_centres_in_rcs.groupby('object_id').size().reset_index(name='health_centres_count')
 
-health_centres_count.to_csv(cwd+'/Sources/BHARATMAPS/data/healthcentres_per_rc.csv', index=False)
+health_centres_count.to_csv(cwd+'/Sources/BHARATMAPS/data/ModelData/healthcentres_per_rc.csv', index=False)

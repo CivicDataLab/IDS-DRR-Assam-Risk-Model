8a13a9830f16ff6ce574760c3251743f6a818d0d
Sai Krishna <saikrishnadammalapati@gmail.com>
2023-10-17 22:34:56 +0530

    UPDATE TENDERS variables - master

--- Sources/TENDERS/scripts/transformer.py
+++ Sources/TENDERS/scripts/transformer.py
@@ -12,7 +12,7 @@ flood_tenders_geotagged_df = flood_tenders_geotagged_df.merge(assam_rc_gdf,
                                  how='left')
 # Total tender variable
 variable = 'total_tender_awarded_value'
-total_tender_awarded_value_df= flood_tenders_geotagged_df.groupby(['month','DISTRICT_FINALISED','REVENUE_CIRCLE_FINALISED'])[['Awarded Value']].sum().reset_index()
+total_tender_awarded_value_df= flood_tenders_geotagged_df.groupby(['month', 'object_id'])[['Awarded Value']].sum().reset_index()
 
 for year_month in total_tender_awarded_value_df.month.unique():
     variable_df_monthly = total_tender_awarded_value_df[total_tender_awarded_value_df.month == year_month]
@@ -26,7 +26,7 @@ for year_month in total_tender_awarded_value_df.month.unique():
 variables = flood_tenders_geotagged_df['Scheme'].unique()
 for variable in variables:
     variable_df = flood_tenders_geotagged_df[flood_tenders_geotagged_df['Scheme'] == variable]
-    variable_df= variable_df.groupby(['month','DISTRICT_FINALISED','REVENUE_CIRCLE_FINALISED'])[['Awarded Value']].sum().reset_index()
+    variable_df= variable_df.groupby(['month', 'object_id'])[['Awarded Value']].sum().reset_index()
 
     variable = str(variable) + '_tenders_awarded_value'
     for year_month in variable_df.month.unique():
@@ -42,7 +42,7 @@ for variable in variables:
 variables = flood_tenders_geotagged_df['Response Type'].unique()
 for variable in variables:
     variable_df = flood_tenders_geotagged_df[flood_tenders_geotagged_df['Response Type'] == variable]
-    variable_df= variable_df.groupby(['month','DISTRICT_FINALISED','REVENUE_CIRCLE_FINALISED'])[['Awarded Value']].sum().reset_index()
+    variable_df= variable_df.groupby(['month', 'object_id'])[['Awarded Value']].sum().reset_index()
 
     variable = str(variable) + '_tenders_awarded_value'
     for year_month in variable_df.month.unique():

445777414d84c8ac02e3fa9a70fa3d1a658dfcc7
Sai Krishna <saikrishnadammalapati@gmail.com>
2023-10-17 16:38:07 +0530

    FRIMS UPDATE process data dump till Aug31

--- Sources/master.py
+++ Sources/master.py
@@ -2,6 +2,29 @@ import os
 import glob
 import pandas as pd
 import re
+main_directory = os.getcwd()+'/Sources/'
+
+# Iterate through all folders and sub-folders
+for root, dirs, files in os.walk(main_directory):
+    if 'variables/' in root:
+        csv_files = glob.glob(root+'/*.csv')
+        dfs= []
+        for csv in csv_files:
+            month = re.findall(r'\d{4}_\d{2}', csv)[0]
+            df = pd.read_csv(csv)
+            df['month'] = month
+            dfs.append(df)
+
+        master_df = pd.concat(dfs)
+        master_df.to_csv(main_directory+'master/{}.csv'.format(csv_files[0].split('/')[-1][:-12]),
+                         index=False)
+
+    else:
+        pass
+    
+            
+
+exit()
 #IMD
 path = os.getcwd()+'/Sources/IMD/data/rain/csv/'
 csvs = glob.glob(path+'*.csv')
